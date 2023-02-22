b84f1ac7f7b1ec2a62576102954e2b917b5c86f5
Tien Tran <tien@transfamily.info>
2023-02-23 10:57:40 +1100

    Ensure zero code shutdown

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-set -e
 
 # credits: https://medium.com/@cotton_ori/how-to-terminate-a-side-car-container-in-kubernetes-job-2468f435ca99
 
 if [[ -n "$TERMINATE_PATH" ]]; then
+  set -e
   cloud_sql_proxy "$@" & CHILD_PID=$!
+  set +e
   (while true; do if [[ -f "$TERMINATE_PATH" ]]; then kill $CHILD_PID; echo "Shutting down..."; fi; sleep 1; done) &
   wait $CHILD_PID
   if [[ -f "$TERMINATE_PATH" ]]; then echo "Shutdown completed. Exiting..."; exit 0; fi
